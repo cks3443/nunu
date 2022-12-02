92be4a77ff892965a742573a6e0c6ba55397b8f2
ks choi <cks3443@gmail.com>
2022-12-02 11:05:45 +0900

    view1 페이지 렌더링

--- src/main/kotlin/com/mustard/nunu/view/ViewController.kt
+++ src/main/kotlin/com/mustard/nunu/view/ViewController.kt
@@ -1,9 +1,11 @@
 package com.mustard.nunu.view
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/view")
@@ -12,11 +14,29 @@ class ViewController {
 
     val VIEW_1 = "view1"
 
-    @GetMapping("/1")
+    val VIEW_2 = "view2"
+
+    @GetMapping("/1/{id}")
     fun getView1(
-        @RequestParam id: Long,
+
+        @PathVariable id: Long,
+        model: Model,
     ): String {
 
+        model["id"] = id
+
         return VIEW_1
+    }
+
+    @GetMapping("/2/{id}")
+    fun getView2(
+
+        @PathVariable id: Long,
+        model: Model,
+    ): String {
+
+        model["id"] = id
+
+        return VIEW_2
     }
 }
