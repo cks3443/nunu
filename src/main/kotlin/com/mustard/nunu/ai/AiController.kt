3c00f3b587a0ec99ea0fbbf6d5569261a11c6e23
ks choi <cks3443@gmail.com>
2023-01-06 00:52:24 +0900

    NU-12 NU-13

---
+++ src/main/kotlin/com/mustard/nunu/ai/AiController.kt
@@ -0,0 +1,29 @@
+package com.mustard.nunu.ai
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.io.IOException
+import kotlin.jvm.Throws
+
+@Controller
+@RequestMapping("/ai")
+class AiController(
+    private val novelAi: NovelAi,
+    private val papago: Papago,
+) {
+
+    @PostMapping("/generate")
+    @ResponseBody
+    @Throws(IOException::class)
+    fun generateSentence(
+        txt: String,
+    ): String {
+        val kr_en = papago.ko2eng_function(txt)
+        val token = novelAi.getAccessToken()
+        val en_gen = novelAi.translate(token, kr_en)
+        val en_kr = papago.eng2ko_function(en_gen)
+        return en_kr
+    }
+}
